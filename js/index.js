b7b5bc6e519b081f43d1f68d9a6a2f2603e7c7c7
baer1 <videocreator@outlook.de>
2024-07-07 00:24:47 +0200

    Clean up Remove code from v1 that will not be used in v2. It's supposed to be a complete overhaul of the design. [I am not a designer, so it'll look more clean compared to v1, but still far from perfect.] I also fixed the copyright headers, but that's irrelevant.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 "use strict";
-const VERSION = "v1.3-0.2";
+const VERSION = "v2.0";
 /*
     baer1 website
     Copyright (C) 2024  baer1
@@ -20,103 +20,5 @@ window.addEventListener("load", () => {
     s.sheet.insertRule(`body>#version::after{content: "Script ${VERSION}"}`);
 });
 window.addEventListener("load", () => {
-    let buttons = document
-        .getElementById("content")
-        .getElementsByTagName("button");
-    for (let i = 0; i < buttons.length; i++) {
-        const btn = buttons[i];
-        btn.addEventListener("click", (e) => {
-            transition(btn.getAttribute("data-href"), btn.innerText);
-        });
-    }
-    const TITLE = document.title;
-    const CHARS = "abcdefghijklmnopqrstuvwxyz1234567890#+~*?!\"'\\/%[]{}$ .,-_<>|".split("");
-    let title_effect_interval = undefined;
-    function title_effect_frame() {
-        let t = document.title.split("");
-        let i = Math.floor(Math.random() * t.length);
-        t[i] = CHARS[Math.floor(Math.random() * CHARS.length)];
-        t[i] = Math.random() > 0.5 ? t[i].toUpperCase() : t[i];
-        document.title = t.join("");
-    }
-    let stop_title_effect = () => {
-        clearInterval(title_effect_interval);
-        document.title = TITLE;
-    };
-    let start_title_effect = () => {
-        stop_title_effect();
-        title_effect_interval = setInterval(title_effect_frame, 50);
-        title_effect_frame();
-    };
-    window.addEventListener("blur", stop_title_effect);
-    window.addEventListener("focus", start_title_effect);
-    start_title_effect();
-    window.addEventListener("beforeunload", () => {
-        stop_title_effect();
-        return true;
-    });
-});
-function transition(url, displayname) {
-    if (document.body.getAttribute("transition-state"))
-        return false;
-    document.body.setAttribute("transition-state", "");
-    let loader_node = document.getElementById("transition-loader"), continue_node = document.getElementById("transition-continue"), return_node = document.getElementById("transition-return");
-    let preloader = document.createElement("iframe");
-    [document.body, loader_node, continue_node, return_node].forEach((e) => {
-        e.classList.add("transition");
-    });
-    loader_node.childNodes.forEach((e_node) => {
-        let e = e_node;
-        e.onanimationiteration = () => {
-            if (preloader.hasAttribute("transition-finished")) {
-                e.classList.add("transition-finished-anim");
-                loader_node.onanimationiteration = null;
-            }
-        };
-    });
-    continue_node.href = url;
-    if (!displayname) {
-        continue_node.innerText = new URL(url).hostname;
-    }
-    else {
-        continue_node.innerHTML = displayname;
-    }
-    preloader.src = url;
-    preloader.addEventListener("load", () => {
-        preloader.setAttribute("transition-finished", "");
-        [loader_node, continue_node, return_node].forEach((e) => {
-            e.classList.add("transition-finished");
-        });
-        setTimeout(() => {
-            document.body.removeAttribute("transition-state");
-            location.href = url;
-        }, 750);
-    });
-    preloader.className = "preloader";
-    document.body.appendChild(preloader);
-}
-function cancel_transition(e) {
-    if (document.body.hasAttribute("transition-state"))
-        return false;
-    e === null || e === void 0 ? void 0 : e.preventDefault();
-    let loader_node = document.getElementById("transition-loader");
-    [
-        document.body,
-        loader_node,
-        document.getElementById("transition-continue"),
-        document.getElementById("transition-return"),
-    ].forEach((e) => {
-        e.classList.remove("transition");
-    });
-    loader_node.childNodes.forEach((e_node) => {
-        let e = e_node;
-        e.onanimationiteration = null;
-        e.classList.remove("transition-finished-anim");
-    });
-    loader_node.classList.remove("transition-finished");
-    document.body.removeAttribute("transition-state");
-}
-window.addEventListener("pageshow", (e) => {
-    if (e.persisted)
-        window.location.reload();
+    // New website code here
 });
