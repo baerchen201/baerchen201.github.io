38b02ebb46a6b649b88e3fbb6f5dc06c4b44b4d1
baer1 <videocreator@outlook.de>
2024-07-07 17:14:39 +0200

    Unfinished, I'll finish it later. I'm pretty tired of programming this by now, so I'm just saving my work before going do something else.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 var VERSION = "v2.0";
 /*
     baer1 website
@@ -18,6 +56,124 @@ window.addEventListener("load", function () {
     document.head.appendChild(s);
     s.sheet.insertRule("#version::after{content: \"Script ".concat(VERSION, "\"}"));
 });
+var STEAM_RELAY = "https://still-wood-a68b.videocreator.workers.dev/", STEAM_DEFAULTS = {
+    name: "Georg M. H.",
+    user: "videocreator",
+    id: "76561199245129581",
+    picture: "/me/0.png",
+};
+var STEAM_EXAMPLE_RESPONSES = [
+    {
+        response: {
+            players: [
+                {
+                    steamid: "76561199245129581",
+                    communityvisibilitystate: 3,
+                    profilestate: 1,
+                    personaname: "videocreator",
+                    profileurl: "https://steamcommunity.com/id/baerchen201/",
+                    avatar: "https://avatars.steamstatic.com/76c4df28062c408667bf2279276bc6b9f068dd0b.jpg",
+                    avatarmedium: "https://avatars.steamstatic.com/76c4df28062c408667bf2279276bc6b9f068dd0b_medium.jpg",
+                    avatarfull: "https://avatars.steamstatic.com/76c4df28062c408667bf2279276bc6b9f068dd0b_full.jpg",
+                    avatarhash: "76c4df28062c408667bf2279276bc6b9f068dd0b",
+                    lastlogoff: 1720350940,
+                    personastate: 1,
+                    realname: "Georg M. Hamburg",
+                    primaryclanid: "103582791429521408",
+                    timecreated: 1645258954,
+                    personastateflags: 0,
+                    loccountrycode: "DE",
+                    locstatecode: "01",
+                },
+            ],
+        },
+    },
+    {
+        response: {
+            players: [
+                {
+                    steamid: "76561199245129581",
+                    communityvisibilitystate: 3,
+                    profilestate: 1,
+                    personaname: "videocreator",
+                    profileurl: "https://steamcommunity.com/id/baerchen201/",
+                    avatar: "https://avatars.steamstatic.com/76c4df28062c408667bf2279276bc6b9f068dd0b.jpg",
+                    avatarmedium: "https://avatars.steamstatic.com/76c4df28062c408667bf2279276bc6b9f068dd0b_medium.jpg",
+                    avatarfull: "https://avatars.steamstatic.com/76c4df28062c408667bf2279276bc6b9f068dd0b_full.jpg",
+                    avatarhash: "76c4df28062c408667bf2279276bc6b9f068dd0b",
+                    lastlogoff: 1720350940,
+                    personastate: 1,
+                    realname: "Georg M. Hamburg",
+                    primaryclanid: "103582791429521408",
+                    timecreated: 1645258954,
+                    personastateflags: 0,
+                    gameextrainfo: "Banana",
+                    gameid: "2923300",
+                    loccountrycode: "DE",
+                    locstatecode: "01",
+                },
+            ],
+        },
+    },
+];
+function create_steam_game_node(appid) {
+    return __awaiter(this, void 0, void 0, function () {
+        var response, game_info, game_node;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, fetch("https://store.steampowered.com/api/appdetails?appids=".concat(appid))];
+                case 1:
+                    response = _a.sent();
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    game_info = (_a.sent())[appid];
+                    if (!game_info["success"])
+                        return [2 /*return*/, null];
+                    game_node = document.createElement("div");
+                    if (game_info.data.header_image)
+                        game_node.style.backgroundImage = "url(".concat(game_info.data.header_image, ")");
+                    else if (game_info.data.capsule_image)
+                        game_node.style.backgroundImage = "url(".concat(game_info.data.capsule_image, ")");
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+function get_steam_profile() {
+    return fetch(STEAM_RELAY);
+}
 window.addEventListener("load", function () {
-    // No new website scripts yet
+    var steam_mini_profile = document.getElementById("steam-profile");
+    var steam_profile_nodes = {
+        picture: steam_mini_profile.querySelector("img"),
+        user: steam_mini_profile.getElementsByTagName("div")[0],
+        name: steam_mini_profile.getElementsByTagName("div")[1],
+        status: steam_mini_profile.getElementsByTagName("div")[2],
+    };
+    function set_steam_status(status, profile) {
+        if (profile === void 0) { profile = STEAM_DEFAULTS; }
+        switch (status) {
+            case null:
+                steam_profile_nodes.picture.src = profile.picture;
+                steam_profile_nodes.user.innerText = profile.user;
+                break;
+            default:
+                break;
+        }
+    }
+    get_steam_profile().then(function (r) { return __awaiter(void 0, void 0, void 0, function () {
+        var json;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    if (!r.ok)
+                        set_steam_status(null);
+                    return [4 /*yield*/, r.json()];
+                case 1:
+                    json = _a.sent();
+                    set_steam_status(json.response.players[0]);
+                    return [2 /*return*/];
+            }
+        });
+    }); });
 });
--- js/index.ts
+++ js/index.ts
@@ -1,3 +1,5 @@
+import { deprecate } from "util";
+
 const VERSION: string = "v2.0";
 /*
     baer1 website
@@ -20,6 +22,224 @@ window.addEventListener("load", () => {
   s.sheet!.insertRule(`#version::after{content: "Script ${VERSION}"}`);
 });
 
+interface STEAM_PROFILE {
+  user: string;
+  name: string;
+  id: string;
+  picture: string;
+}
+
+const STEAM_RELAY: string = "https://still-wood-a68b.videocreator.workers.dev/",
+  STEAM_DEFAULTS: STEAM_PROFILE = {
+    name: "Georg M. H.",
+    user: "videocreator",
+    id: "76561199245129581",
+    picture: "/me/0.png",
+  };
+const STEAM_EXAMPLE_RESPONSES: Object[] = [
+  {
+    response: {
+      players: [
+        {
+          steamid: "76561199245129581",
+          communityvisibilitystate: 3,
+          profilestate: 1,
+          personaname: "videocreator",
+          profileurl: "https://steamcommunity.com/id/baerchen201/",
+          avatar:
+            "https://avatars.steamstatic.com/76c4df28062c408667bf2279276bc6b9f068dd0b.jpg",
+          avatarmedium:
+            "https://avatars.steamstatic.com/76c4df28062c408667bf2279276bc6b9f068dd0b_medium.jpg",
+          avatarfull:
+            "https://avatars.steamstatic.com/76c4df28062c408667bf2279276bc6b9f068dd0b_full.jpg",
+          avatarhash: "76c4df28062c408667bf2279276bc6b9f068dd0b",
+          lastlogoff: 1720350940,
+          personastate: 1,
+          realname: "Georg M. Hamburg",
+          primaryclanid: "103582791429521408",
+          timecreated: 1645258954,
+          personastateflags: 0,
+          loccountrycode: "DE",
+          locstatecode: "01",
+        },
+      ],
+    },
+  },
+  {
+    response: {
+      players: [
+        {
+          steamid: "76561199245129581",
+          communityvisibilitystate: 3,
+          profilestate: 1,
+          personaname: "videocreator",
+          profileurl: "https://steamcommunity.com/id/baerchen201/",
+          avatar:
+            "https://avatars.steamstatic.com/76c4df28062c408667bf2279276bc6b9f068dd0b.jpg",
+          avatarmedium:
+            "https://avatars.steamstatic.com/76c4df28062c408667bf2279276bc6b9f068dd0b_medium.jpg",
+          avatarfull:
+            "https://avatars.steamstatic.com/76c4df28062c408667bf2279276bc6b9f068dd0b_full.jpg",
+          avatarhash: "76c4df28062c408667bf2279276bc6b9f068dd0b",
+          lastlogoff: 1720350940,
+          personastate: 1,
+          realname: "Georg M. Hamburg",
+          primaryclanid: "103582791429521408",
+          timecreated: 1645258954,
+          personastateflags: 0,
+          gameextrainfo: "Banana",
+          gameid: "2923300",
+          loccountrycode: "DE",
+          locstatecode: "01",
+        },
+      ],
+    },
+  },
+];
+
+interface STEAM_PROFILE_RAW {
+  response: {
+    players: {
+      steamid: string;
+      communityvisibilitystate: number;
+      profilestate: number;
+      personaname: string;
+      profileurl: string;
+      avatar: string;
+      avatarmedium: string;
+      avatarfull: string;
+      avatarhash: string;
+      lastlogoff: number;
+      personastate: number;
+      realname: string;
+      primaryclanid: string;
+      timecreated: number;
+      personastateflags: number;
+      gameextrainfo?: string;
+      gameid?: string;
+      loccountrycode: string;
+      locstatecode: string;
+    }[];
+  };
+}
+
+interface STEAM_GAME_INFO {
+  success: boolean;
+  data: {
+    type: string;
+    name: string;
+    steam_appid: number;
+    required_age: number;
+    is_free: boolean;
+    dlc: number[];
+    detailed_description: string;
+    about_the_game: string;
+    short_description: string;
+    supported_languages: string;
+    reviews: string;
+    header_image: string;
+    capsule_image: string;
+    capsule_imagev5: string;
+    website: string;
+    pc_requirements: {
+      minimum: string;
+    };
+    mac_requirements: {
+      minimum: string;
+    };
+    linux_requirements: {
+      minimum: string;
+    };
+    developers: string[];
+    publishers: string[];
+    price_overview: any;
+    packages: number[];
+    package_groups: any;
+    platforms: {
+      windows: boolean;
+      mac: boolean;
+      linux: boolean;
+    };
+    categories: {
+      id: number;
+      description: string;
+    }[];
+    genres: {
+      id: string;
+      description: string;
+    }[];
+    screenshots: {
+      id: number;
+      path_thumbnail: string;
+      path_full: string;
+    }[];
+    movies: {
+      id: number;
+      name: string;
+      thumbnail: string;
+      highlight: true;
+    }[];
+    recommendations: any;
+    achievements: any;
+    release_date: {
+      coming_soon: boolean;
+      date: string;
+    };
+    support_info: any;
+    background: string;
+    background_raw: string;
+    content_descriptors: any;
+    ratings: any;
+  };
+}
+
+async function create_steam_game_node(appid: number) {
+  let response = await fetch(
+    `https://store.steampowered.com/api/appdetails?appids=${appid}`
+  );
+
+  let game_info: STEAM_GAME_INFO = (await response.json())[appid];
+  if (!game_info["success"]) return null;
+
+  let game_node: HTMLDivElement = document.createElement("div");
+  if (game_info.data.header_image)
+    game_node.style.backgroundImage = `url(${game_info.data.header_image})`;
+  else if (game_info.data.capsule_image)
+    game_node.style.backgroundImage = `url(${game_info.data.capsule_image})`;
+  /* Ok I found out how to embed steam store widgets in iframes, so there go the last 2 hours of my life. */
+}
+
+function get_steam_profile(): Promise<Response> {
+  return fetch(STEAM_RELAY);
+}
+
 window.addEventListener("load", () => {
-  // No new website scripts yet
+  let steam_mini_profile: HTMLDivElement = document.getElementById(
+    "steam-profile"
+  ) as HTMLDivElement;
+  let steam_profile_nodes = {
+    picture: steam_mini_profile.querySelector("img") as HTMLImageElement,
+    user: steam_mini_profile.getElementsByTagName("div")[0],
+    name: steam_mini_profile.getElementsByTagName("div")[1],
+    status: steam_mini_profile.getElementsByTagName("div")[2],
+  };
+  function set_steam_status(
+    status: null | 0 | 1 | 2 | { name: string; url: string },
+    profile: STEAM_PROFILE = STEAM_DEFAULTS
+  ) {
+    switch (status) {
+      case null:
+        steam_profile_nodes.picture.src = profile.picture;
+        steam_profile_nodes.user.innerText = profile.user;
+        break;
+
+      default:
+        break;
+    }
+  }
+  get_steam_profile().then(async (r: Response) => {
+    if (!r.ok) set_steam_status(null);
+    let json: STEAM_PROFILE_RAW = await r.json();
+    set_steam_status(json.response.players[0]);
+  });
 });
