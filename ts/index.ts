c49e3406dffdf24b0f4426a91668c9e4b2bcb237
baer1 <videocreator@outlook.de>
2024-10-12 23:58:59 +0200

    Replace old page transition
    The old one was too gimmicky, buggy and too spaghetticode to keep.
    The new one is modern, simple, not buggy (hopefully) and has clean code.

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,4 @@
-const VERSION: string = "v1.3-0.4";
+const VERSION: string = "v1.3-0.5";
 /*
     baer1 website
     Copyright (C) 2024  baer1
@@ -26,83 +26,25 @@ window.addEventListener("load", () => {
     .getElementsByTagName("button");
   for (let i = 0; i < buttons.length; i++) {
     const btn: HTMLButtonElement = buttons[i];
-    btn.addEventListener("click", (e: MouseEvent) => {
-      transition(btn.getAttribute("data-href")!, btn.innerText);
+    btn.addEventListener("click", () => {
+      let href = btn.getAttribute("data-href");
+      if (!href)
+        return alert(
+          "This button does not have a data-href attribute.\nPlease open an issue on GitHub to get this fixed"
+        );
+      document.body.style.pointerEvents = "none";
+      document.body
+        .animate([{ opacity: "1" }, { opacity: "0" }], {
+          duration: 500,
+          easing: "ease-in",
+          fill: "forwards",
+        })
+        .addEventListener("finish", () => {
+          location.href = href;
+        });
     });
   }
 });
-
-function transition(url: string, displayname?: string) {
-  if (document.body.getAttribute("transition-state")) return false;
-  document.body.setAttribute("transition-state", "");
-  let loader_node: HTMLDivElement = document.getElementById(
-      "transition-loader"
-    ) as HTMLDivElement,
-    continue_node: HTMLLinkElement = document.getElementById(
-      "transition-continue"
-    ) as HTMLLinkElement,
-    return_node: HTMLLinkElement = document.getElementById(
-      "transition-return"
-    ) as HTMLLinkElement;
-  let preloader: HTMLIFrameElement = document.createElement("iframe");
-  [document.body, loader_node, continue_node, return_node].forEach(
-    (e: HTMLElement) => {
-      e.classList.add("transition");
-    }
-  );
-  loader_node.childNodes.forEach((e_node: ChildNode) => {
-    let e: HTMLElement = e_node as HTMLElement;
-    e.onanimationiteration = () => {
-      if (preloader.hasAttribute("transition-finished")) {
-        e.classList.add("transition-finished-anim");
-        loader_node.onanimationiteration = null;
-      }
-    };
-  });
-  continue_node.href = url;
-  if (!displayname) {
-    continue_node.innerText = new URL(url).hostname;
-  } else {
-    continue_node.innerHTML = displayname;
-  }
-  preloader.src = url;
-  preloader.addEventListener("load", () => {
-    preloader.setAttribute("transition-finished", "");
-    [loader_node, continue_node, return_node].forEach((e: HTMLElement) => {
-      e.classList.add("transition-finished");
-    });
-
-    setTimeout(() => {
-      document.body.removeAttribute("transition-state");
-      location.href = url;
-    }, 750);
-  });
-  preloader.className = "preloader";
-  document.body.appendChild(preloader);
-}
-
-function cancel_transition(e?: Event) {
-  if (document.body.hasAttribute("transition-state")) return false;
-  e?.preventDefault();
-  let loader_node: HTMLDivElement = document.getElementById(
-    "transition-loader"
-  ) as HTMLDivElement;
-  [
-    document.body,
-    loader_node,
-    document.getElementById("transition-continue") as HTMLLinkElement,
-    document.getElementById("transition-return") as HTMLLinkElement,
-  ].forEach((e: Element) => {
-    e.classList.remove("transition");
-  });
-  loader_node.childNodes.forEach((e_node: ChildNode) => {
-    let e: HTMLElement = e_node as HTMLElement;
-    e.onanimationiteration = null;
-    e.classList.remove("transition-finished-anim");
-  });
-  loader_node.classList.remove("transition-finished");
-  document.body.removeAttribute("transition-state");
-}
 
 window.addEventListener("pageshow", (e: PageTransitionEvent) => {
   if (e.persisted) window.location.reload();
